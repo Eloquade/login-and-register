bb84706a221a1033b1b022e18d646a545b21ba1d
Jheave <jimenezjheave123@gmail.com>
2021-04-05 22:17:25 +0800

    fix email

--- app/views/registrationForm.php
+++ app/views/registrationForm.php
@@ -41,7 +41,7 @@
          </div>
           <div class="inputfield">
             <label>Email</label>
-            <input type="text" name = "EmailAddress" class="input" autocomplete="off">
+            <input type="email" name = "EmailAddress" class="input" autocomplete="off">
          </div>
         <div class="inputfield">
             <label>Phone Number</label>
