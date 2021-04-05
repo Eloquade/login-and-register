d27b69db770c1d4745b9c71c141b1c07bc450883
Jheave <jimenezjheave123@gmail.com>
2021-04-05 22:14:50 +0800

    fix autocomplete=off

--- app/views/registrationForm.php
+++ app/views/registrationForm.php
@@ -41,7 +41,7 @@
          </div>
           <div class="inputfield">
             <label>Email</label>
-            <input type="text" name = "EmailAddress" class="input">
+            <input type="text" name = "EmailAddress" class="input" autocomplete="off">
          </div>
         <div class="inputfield">
             <label>Phone Number</label>
